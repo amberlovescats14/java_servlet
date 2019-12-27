3532ac46fa4f5b1d3a252761dccfac645e529ada
Amber Jones <amberlovescats14@gmail.com>
2019-12-27 11:40:09 -0600

    trying to get the dispatcher to work

--- src/main/java/WebAppServlet.java
+++ src/main/java/WebAppServlet.java
@@ -7,15 +7,16 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 //localhost:8080/webapp
-@WebServlet(name = "WebAppServlet", urlPatterns = "/webapp")public class WebAppServlet extends HttpServlet {
+@WebServlet(name = "WebAppServlet", urlPatterns = "/")public class WebAppServlet extends HttpServlet {
 
     //GET doGet
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        super.doGet(req, res);
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
         out.println("<h1>Hello, World!</h1>");
+
+//        req.getRequestDispatcher("index.jsp").forward(req, res);
     }
 
     //POST  doPost
